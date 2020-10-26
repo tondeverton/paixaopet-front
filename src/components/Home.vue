ad93a7924c323eda11023f5143a2221c60b6fb57
Everton Luis Santos Pereira <everton.pereira@picpay.com>
2020-10-26 08:42:10 -0300

    components/Home: created

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <header>
+        <h1>PaixãoPet</h1>
+
+        <nav style="display:none">
+            <ul>
+                <li><a href="">Entrar</a></li>
+                <li><a href="">Cadastrar</a></li>
+                <li><a href="">Pets</a></li>
+                <li><a href="">Sobre nós</a></li>
+                <li><a href="">Trabalhe conosco</a></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
